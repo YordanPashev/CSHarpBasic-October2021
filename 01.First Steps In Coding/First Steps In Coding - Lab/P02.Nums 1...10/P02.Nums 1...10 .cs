dbe8e7b214563fdf14871735bc6cfa0bac50f7b3
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-08-17 12:07:07 +0300

    Adding All Problems Descriptions and Solutions to P01 - 03

---
+++ 01.First Steps In Coding/First Steps In Coding - Lab/P02.Nums 1...10/P02.Nums 1...10 .cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace DemoProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                for (int x = 1; x <= 10; x++)
+                    Console.WriteLine(x);
+        }
+    }
+}
